f63d0100db83fc4284e716ff7268acefca66e885
Ariano Wongsosetro <arianowong@gmail.com>
2023-05-18 23:26:01 +0200

    Fixed localhost issue

--- puppiesFullstack/client/src/App.tsx
+++ puppiesFullstack/client/src/App.tsx
@@ -1,33 +1,209 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+interface Puppy {
+  id: number;
+  breed: string;
+  name: string;
+  birth_date: string;
+}
+
+const App = () => {
+  const [puppies, setPuppies] = useState<Puppy[]>([]);
+  const [selectedPuppy, setSelectedPuppy] = useState<Puppy | null>(null);
+  const [name, setName] = useState('');
+  const [breed, setBreed] = useState('');
+  const [birthDate, setBirthDate] = useState('');
+  const [showEditForm, setShowEditForm] = useState(false);
+  const [editedName, setEditedName] = useState('');
+  const [editedBreed, setEditedBreed] = useState('');
+  const [editedBirthDate, setEditedBirthDate] = useState('');
+
+
+  useEffect(() => {
+    fetchPuppies();
+  }, [puppies]);
+
+  const fetchPuppies = async () => {
+    try {
+      const res = await fetch('http://localhost:3000/api/puppies');
+      const data = await res.json();
+      setPuppies(data);
+    } catch (err) {
+      console.error('Error fetching puppies', err);
+    }
+  }
+
+  const handleSelectPuppy = (puppy: Puppy) => {
+    setShowEditForm(false);
+    if (selectedPuppy && selectedPuppy.id === puppy.id) {
+      setSelectedPuppy(null);
+    } else {
+      setSelectedPuppy(puppy);
+    }
+  }
+
+  const handleDeletePuppy = async (puppyId: number) => {
+    try {
+      await fetch(`http://localhost:3000/api/puppies/${puppyId}`, {
+        method: 'DELETE'
+      });
+      setSelectedPuppy(null);
+    } catch (err) {
+      console.error('Error while deleting puppy', err);
+    }
+  }
+
+  const handleAddPuppy = async () => {
+    try {
+      await fetch(`http://localhost:3000/api/puppies`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          breed: breed,
+          name: name,
+          birth_date: birthDate
+        })
+      });
+
+    } catch (err) {
+      console.error('Error while adding puppy', err);
+    }
+  }
+
+  const handleEditPuppy = async (puppyId: number) => {
+    try {
+      const updatedPuppy = {
+        ...selectedPuppy,
+        breed: editedBreed,
+        name: editedName,
+        birth_date: editedBirthDate
+      }
+
+      await fetch(`http://localhost:3000/api/puppies/${puppyId}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(updatedPuppy)
+      });
+
+    } catch (err) {
+      console.error('Error while editing puppy', err);
+    }
+  }
+
+  const handleEditForm = () => {
+    setShowEditForm(!showEditForm);
+  }
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>Puppies</h1>
+        {puppies.length === 0 ? (
+          <p>No puppies found</p>
+        ) : (
+          puppies.map((puppy) => (
+            <div key={puppy.id}>
+              <h2 onClick={() => handleSelectPuppy(puppy)}>{puppy.name}</h2>
+              {selectedPuppy && selectedPuppy.id === puppy.id && (
+                <div>
+                  <h3>Details</h3>
+                  <p>Breed: {puppy.breed}</p>
+                  <p>Birth Date: {puppy.birth_date}</p>
+                  <div>
+                    <button onClick={() => handleEditForm()}>Edit</button>
+                    <button onClick={() => handleDeletePuppy(puppy.id)}>Delete</button>
+                  </div>
+                  <br />
+                  {showEditForm && (
+                    <div>
+                      <div>
+                        <label htmlFor="">Name: </label>
+                        <input
+                          type="text"
+                          value={editedName}
+                          onChange={e => setEditedName(e.target.value)}
+                        />
+                      </div>
+
+                      <br/>
+
+                      <div>
+                        <label htmlFor="">Breed: </label>
+                        <input
+                          type="text"
+                          value={editedBreed}
+                          onChange={e => setEditedBreed(e.target.value)}
+                        />
+                      </div>
+
+                      <br/>
+
+                      <div>
+                        <label htmlFor="">Birth Date: </label>
+                        <input
+                          type="text"
+                          value={editedBirthDate}
+                          onChange={e => setEditedBirthDate(e.target.value)}
+                        />
+                      </div>
+
+                      <br />
+
+                      <button onClick={() => handleEditPuppy(puppy.id)}>Edit Puppy</button>
+                    </div>
+                  )}
+                </div>
+              )}
+            </div>
+          ))
+        )}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <br />
+      
+      <hr/>
+      <div>
+        <h1>Add a puppy</h1>
+
+        <div>
+          <label htmlFor="">Name: </label>
+          <input
+            type="text"
+            value={name}
+            onChange={e => setName(e.target.value)}
+          />
+        </div>
+
+        <br/>
+
+        <div>
+          <label htmlFor="">Breed: </label>
+          <input
+            type="text"
+            value={breed}
+            onChange={e => setBreed(e.target.value)}
+          />
+        </div>
+
+        <br/>
+
+        <div>
+          <label htmlFor="">Birth Date: </label>
+          <input
+            type="text"
+            value={birthDate}
+            onChange={e => setBirthDate(e.target.value)}
+          />
+        </div>
+
+        <br />
+
+        <button onClick={handleAddPuppy}>Add Puppy</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
--- puppiesFullstack/server/server.ts
+++ puppiesFullstack/server/server.ts
@@ -1,32 +1,102 @@
-import express, { Request, Response } from 'express';
-import { MongoClient } from 'mongodb';
+import express, { Request, Response, application } from 'express';
+import { v4 as uuidv4 } from 'uuid';
+import bodyParser from 'body-parser';
+import cors from 'cors';
 
 const app = express();
 const port = 3000;
+app.use(cors());
 
-const uri = 'mongodb://localhost:27017/';
-const dbName = 'puppydb';
+interface Puppy {
+  id: string;
+  breed: string;
+  name: string;
+  birth_date: string;
+}
 
-app.get('/puppies', async (req: Request, res: Response) => {
-  try {
-    const client = new MongoClient(uri);
+let puppies: Puppy[] = [
+  {
+    id: uuidv4(),
+    breed: 'Golden Retriever',
+    name: 'Bob',
+    birth_date: '19-02-2008'
+  },
+  {
+    id: uuidv4(),
+    breed: 'Bulldog',
+    name: 'Samson',
+    birth_date: '01-05-2001'
+  },
+  {
+    id: uuidv4(),
+    breed: 'Dachshund',
+    name: 'Gertje',
+    birth_date: '07-05-2012'
+  },
+]
 
-    await client.connect();
-    console.log('Connected successfully to server');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-    const db = client.db(dbName);
-    const collection = db.collection('puppies');
+app.get('/', (_req: Request, res: Response) => {
+  res.send('Hello World!');
+});
 
-    const puppies = await collection.find().toArray();
+app.get('/api/puppies', (_req: Request, res: Response) => {
+  res.json(puppies);
+});
 
+app.get('/api/puppies/:id', (req: Request, res: Response) => {
+  const id = req.params.id;
+  const puppy = puppies.find(p => p.id === id);
+
+  if (puppy) {
+    res.json(puppy);
+  } else {
+    res.status(404)
+      .json({ message: 'Puppy not found' });
+  }
+});
+
+app.post('/api/puppies', (req: Request, res: Response) => {
+  const { breed, name, birth_date } = req.body;
+
+  const puppy: Puppy = { id: uuidv4(), breed, name, birth_date};
+
+  puppies.push(puppy);
+
+  res.status(201).json(puppies);
+});
+
+app.put('/api/puppies/:id', (req: Request, res: Response) => {
+  const id = req.params.id;
+  const { breed, name, birth_date } = req.body;
+  const index = puppies.findIndex(p => p.id === id);
+
+  if (index >= 0) {
+    puppies[index] = { ...puppies[index], breed, name, birth_date };
     res.json(puppies);
+  } else {
+    res
+      .status(404)
+      .json({ message: 'Puppy not found'});
+  }
+});
 
-  } catch (err) {
-    console.error('Error:', err);
-    res.status(500).json({ error: 'Internal Server Error' });
+app.delete('/api/puppies/:id', (req: Request, res: Response) => {
+  const id = req.params.id;
+  const index = puppies.findIndex(p => p.id === id);
+
+  if (index >= 0) {
+    puppies.splice(index, 1);
+    res.json(puppies);
+  } else {
+    res
+      .status(404)
+      .json({ message: 'Puppy not found'});
   }
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server listening on ${port}`);
 });
